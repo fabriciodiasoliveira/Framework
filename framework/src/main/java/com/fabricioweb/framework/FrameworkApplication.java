95c6fae1df391f748a60c3a7135c579665202c04
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-02-08 09:14:50 -0300

    Novo inicializador

--- framework/src/main/java/com/fabricioweb/framework/FrameworkApplication.java
+++ framework/src/main/java/com/fabricioweb/framework/FrameworkApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.fabricioweb.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FrameworkApplication.class, args);
 	}
 
 }
--- framework/src/test/java/com/fabricioweb/framework/FrameworkApplicationTests.java
+++ framework/src/test/java/com/fabricioweb/framework/FrameworkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.fabricioweb.framework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FrameworkApplicationTests {
 
 	@Test
 	void contextLoads() {
