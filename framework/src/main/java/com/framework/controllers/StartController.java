d49e1d0728d3050964cef78355882fa671f78ba8
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-02-10 09:16:53 -0300

    Alterado o nome do controller

--- framework/src/main/java/com/framework/controllers/StartController.java
+++ framework/src/main/java/com/framework/controllers/StartController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author fabricio
  */
 @RestController
-public class HelloController {
+public class StartController {
 
 	@RequestMapping("/")
 	public String index() {
